b98a62d37becb242d62f5efa60b547b6da2f0b3d
Khachikyan01 <76685753+Khachikyan01@users.noreply.github.com>
2021-02-10 15:54:06 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1026,9 +1026,8 @@ if company and company != "Select a Company":
         title = f"<h1 style='{style}'>'{text1}'</h1>"
         col6_1.write(title, unsafe_allow_html=True)
         col6_2.markdown("---")
-        col6_2.write("""
-       
-        """)
+        col6_2.write(""" """)
+        col6_2.write(""" """)
         title= f"<h1 style='{style}'>'{text2}' </h1>"
         col6_2.write(title, unsafe_allow_html=True)
         
@@ -1067,9 +1066,8 @@ if company and company != "Select a Company":
         title = f"<h1 style='{style}'>'{text1}'</h1>"
         col6_1.write(title, unsafe_allow_html=True)
         col6_2.markdown("---")
-        col6_2.write("""
-       
-        """)
+        col6_2.write(""" """)
+        col6_2.write(""" """)
         title= f"<h1 style='{style}'>'{text2}' </h1>"
         col6_2.write(title, unsafe_allow_html=True)
         
